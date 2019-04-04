925564d396fe05cf0d67b422d82ef042eb1d1b8a
ocampbe8 <47729146+ocampbe8@users.noreply.github.com>
2019-04-04 15:44:50 -0400

    Owen's Screens
    Created basic counselor home screen and linked it with empty listener page to be populated by database when we figure it out.

---
+++ pSquared-app/app/src/main/java/com/example/psquared/ListenerDatabase.java
@@ -0,0 +1,13 @@
+package com.example.psquared;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListenerDatabase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listener_database);
+    }
+}
