da8c6870c15f812defc3af24d50cad9358322dc4
jlee0418 <jonghae.lee.0418@gmail.com>
2019-04-06 15:11:43 -0400

    Added HomeListener, HomeTalker screens

---
+++ pSquared-app/app/src/main/java/com/example/psquared/HomeTalker.java
@@ -0,0 +1,49 @@
+package com.example.psquared;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeTalker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Wait for talker button to be clicked
+        onTalk();
+    }
+
+    /**
+     * Talker presses the Talk button.
+     */
+    public void onTalk() {
+        final Button talkBtn = findViewById(R.id.talk);
+        final TextView tv = findViewById(R.id.pressToStopText);
+        talkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                talkBtn.setAlpha(.5f);
+                talkBtn.setText("Connecting to a Listener...");
+                talkBtn.setTextSize(30);
+                tv.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    /**
+     * Reset button attributes when Talk button is "unclicked".
+     */
+    public void resetTalk() {
+        final Button talkBtn = findViewById(R.id.talk);
+        final TextView tv = findViewById(R.id.pressToStopText);
+
+        talkBtn.setAlpha(1f);
+        talkBtn.setText("Talk");
+        talkBtn.setTextSize(55);
+        tv.setVisibility(View.GONE);
+    }
+}
