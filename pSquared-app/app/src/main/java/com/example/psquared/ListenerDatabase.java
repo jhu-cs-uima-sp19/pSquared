46c7bd80ed495e33dbf3bc759c73bd5a1f90cec7
Owen <ocampbe8@jhu.edu>
2019-04-12 16:52:59 -0400

    Listener Database Update
    Changed the preliminary set method to a toggle method for users based off of the counselor's text input. The counselor types in the name and when they press the button, it should toggle the status of the typed in user between listener and talker.

--- pSquared-app/app/src/main/java/com/example/psquared/ListenerDatabase.java
+++ pSquared-app/app/src/main/java/com/example/psquared/ListenerDatabase.java
@@ -1,14 +1,19 @@
 package com.example.psquared;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ListenerDatabase extends AppCompatActivity {
     private FirebaseDatabase database;
@@ -24,9 +29,30 @@ public class ListenerDatabase extends AppCompatActivity {
         startActivity(listenersToMain);
     }
 
-    public void setListener(View view) {
+    public void toggleListener(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String username = editText.getText().toString();
         DatabaseReference users = database.getReference("Users");
-        DatabaseReference child = users.child("charles");
-        child.setValue(1);
+        final DatabaseReference child = users.child(username);
+        child.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //getting the listener value from the firebase database
+                long value = dataSnapshot.getValue(long.class);
+                Integer v = new Integer((int) value);
+                if (v == 0) {
+                    child.setValue(1);
+                } else if (v == 1) {
+                    child.setValue(0);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
