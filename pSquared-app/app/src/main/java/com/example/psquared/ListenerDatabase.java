30aee7b949210fb9d41dc45d4e4259c71085e8bb
ocampbe8 <47729146+ocampbe8@users.noreply.github.com>
2019-04-12 15:41:33 -0400

    Added preliminary listener setting method

--- pSquared-app/app/src/main/java/com/example/psquared/ListenerDatabase.java
+++ pSquared-app/app/src/main/java/com/example/psquared/ListenerDatabase.java
@@ -1,13 +1,32 @@
 package com.example.psquared;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ListenerDatabase extends AppCompatActivity {
-
+    private FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(getApplicationContext());
         setContentView(R.layout.activity_listener_database);
+        database = FirebaseDatabase.getInstance();
+    }
+    public void backButton(View view) {
+        Intent listenersToMain = new Intent(this, CounselorMain.class);
+        startActivity(listenersToMain);
+    }
+
+    public void setListener(View view) {
+        DatabaseReference users = database.getReference("Users");
+        DatabaseReference child = users.child("charles");
+        child.setValue(1);
     }
 }
