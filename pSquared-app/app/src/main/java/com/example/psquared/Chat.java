6553e508bae5b6118efe61f3b49bc5c7686c27b5
Kaitlyn <penguinkait24@gmail.com>
2019-04-13 19:02:27 -0400

    in the process of making chat anonymous

--- pSquared-app/app/src/main/java/com/example/psquared/Chat.java
+++ pSquared-app/app/src/main/java/com/example/psquared/Chat.java
@@ -3,7 +3,6 @@ package com.example.psquared;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,19 +21,14 @@ import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.Date;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class Chat extends AppCompatActivity {
 
-    private static int SIGN_IN_REQUEST_CODE = 1;
     private FirebaseListAdapter<ChatMessage> adapter;
     RelativeLayout activity_chat;
     FloatingActionButton send;
@@ -45,41 +39,6 @@ public class Chat extends AppCompatActivity {
     private String id;
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_sign_out) {
-            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    Snackbar.make(activity_chat, "You have signed out.", Snackbar.LENGTH_SHORT).show();
-                    finish();
-                }
-            });
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.chat_menu,menu);
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == SIGN_IN_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Snackbar.make(activity_chat, "Signed in. Welcome!", Snackbar.LENGTH_SHORT).show();
-                displayChatMessage();
-            }
-            else {
-                Snackbar.make(activity_chat, "Error signing in.", Snackbar.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
@@ -87,6 +46,7 @@ public class Chat extends AppCompatActivity {
         settings = getDefaultSharedPreferences(this);
         editor = settings.edit();
         id = settings.getString("curChat", "fail");
+        // initialize necessary objects
         activity_chat = (RelativeLayout)findViewById(R.id.activity_chat);
         send = (FloatingActionButton)findViewById(R.id.fabsend);
         exit = (FloatingActionButton)findViewById(R.id.fabexit);
@@ -114,7 +74,6 @@ public class Chat extends AppCompatActivity {
         //load content
         displayChatMessage();
 
-
     }
 
     private void displayChatMessage() {
@@ -126,9 +85,14 @@ public class Chat extends AppCompatActivity {
                 messageText = (TextView) v.findViewById(R.id.message_text);
                 messageUser = (TextView) v.findViewById(R.id.message_user);
                 messageTime = (TextView) v.findViewById(R.id.message_time);
+                String me = settings.getString("name", "unknown");
+                if (messageUser.equals(me)) {
+                    messageUser.setText(model.getMessageUser());
+                } else {
+                    messageUser.setText("Kaitlyn");
+                }
 
                 messageText.setText(model.getMessageText());
-                messageUser.setText(model.getMessageUser());
                 messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm)",model.getMessageTime()));
             }
         };
