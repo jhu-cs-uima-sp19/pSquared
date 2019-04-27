927cb1b47759222407bb67dcbc18f33ff197d58d
Christopher Xu <Christopher.xu@live.com>
2019-04-27 11:12:21 -0400

    Makes sure Listeners and Talkers are unavailable when signing out

--- pSquared-app/app/src/main/java/com/example/psquared/Settings.java
+++ pSquared-app/app/src/main/java/com/example/psquared/Settings.java
@@ -2,6 +2,7 @@ package com.example.psquared;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
@@ -70,12 +76,63 @@ public class Settings extends AppCompatActivity {
 
     // signs the user out and brings user to login screen
     public void signOut(View v) {
+        mAuth.signOut();
+
+        makeUnavailable();
+
         editor.putString("email", "email");
         editor.putString("password", "password");
         editor.commit();
-        mAuth.signOut();
+
         Intent toLogin = new Intent(this, MainActivity.class);
         startActivity(toLogin);
 
+    }
+
+    private void makeUnavailable() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
+
+        //removing from availableTalker list if on list.
+        final DatabaseReference curTalker = database.getReference("availableTalkers");
+        curTalker.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                curTalker.removeEventListener(this);
+                DatabaseReference curUser;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.getValue().toString().equals(email)) {
+                        curUser = curTalker.child(snapshot.getKey());
+                        curUser.removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //removing from availableListener list if on list
+        final DatabaseReference curListener = database.getReference("availableListeners");
+        curListener.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                curListener.removeEventListener(this);
+                DatabaseReference curUser;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.getValue().toString().equals(email.substring(0, email.indexOf("@")))) {
+                        curUser = curListener.child(snapshot.getKey());
+                        curUser.removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
