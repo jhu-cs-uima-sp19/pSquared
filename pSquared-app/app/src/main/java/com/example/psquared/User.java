118bac7cef58662d780963744d222d7aa19293a9
Christopher Xu <Christopher.xu@live.com>
2019-05-01 21:52:19 -0400

    finished recyclerview and added search and filter

--- pSquared-app/app/src/main/java/com/example/psquared/User.java
+++ pSquared-app/app/src/main/java/com/example/psquared/User.java
@@ -1,21 +1,25 @@
 package com.example.psquared;
 
 public class User {
-    public String id, type;
+    public String id, type, active;
 
     // dummy constructor
     public User() {
 
     }
 
-    public User (String idIn, String typeIn) {
+    public User (String idIn, String typeIn, String activeIn) {
         id = idIn;
         type = typeIn;
+        active = activeIn;
     }
     public String getId() {
         System.out.println(id);
         return id;
 
+    }
+    public void setActive(String active) {
+        this.active = active;
     }
 
     public void setId(String id) {
